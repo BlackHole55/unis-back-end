29f1afa114c9a4448b6d8289d2a2a77ba0f38299
Meirhan <syzdykovmr@gmail.com>
2023-10-14 19:16:36 +0600

    first migration

---
+++ database/migrations/2023_10_14_121406_create_universities_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('universities', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('location');
+            $table->text('description')->nullable();
+            $table->string('link_to_website');
+            $table->string('added_timestamp');
+            $table->string('updated_timestamp')->nullable();
+            $table->string('last_changed_admin');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('universities');
+    }
+};
