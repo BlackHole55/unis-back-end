776209c0903b386254b1e974dc10efba22eb1820
Meirhan <syzdykovmr@gmail.com>
2023-10-27 22:13:57 +0600

    Dorm Controller and Model

---
+++ app/Http/Controllers/DormController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Dorm;
+use Carbon\Carbon;
+
+class DormController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $per_page = 10;
+        if($request->has('per_page')){
+            $per_page=$request->per_page;
+        }
+        $dorms = Dorm::paginate($per_page);
+
+        return response([
+            'dorms' => $dorms,
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request, $id)
+    {
+        $university_id = $id;
+        $description = $request->input('description');
+        $location = $request->input('location');
+        $price_tenge = $request->input('price_tenge');
+
+        $adminName = $request->user()->name;
+
+        $date = Carbon::now();
+        $date->timezone('Asia/Almaty');
+        $formattedDate = $date->toIso8601String();
+
+        $dorm = Dorm::create([
+            'university_id' => $university_id,
+            'location' => $location,
+            'description' => $description,
+            'price_tenge' => $price_tenge,
+            'added_timestamp' => $formattedDate,
+            'last_changed_admin' => $adminName,
+        ]);
+
+        return response([
+            'dorm' => $dorm,
+            'status' => 'success',
+        ], 201);
+    }
+    
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return Dorm::where('id', $id)->get();
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $id)
+    {
+        $adminName = $request->user()->name;
+
+        $date = Carbon::now();
+        $date->timezone('Asia/Almaty');
+        $formattedDate = $date->toIso8601String();
+
+        $dorm = Dorm::find($id);
+
+        $dorm->update($request->all());
+
+        $dorm->update([
+            'updated_timestamp' => $formattedDate,
+            'last_changed_admin' =>$adminName,
+        ]);
+
+        return response([
+            'dorm' => $dorm,
+            'status' => 'success',
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        Dorm::destroy($id);
+        return response([
+            'status' => 'success'
+        ], 200);
+    }
+}
--- app/Models/Dorm.php
+++ app/Models/Dorm.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\University;
 
 class Dorm extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'university_id',
+        'description',
+        'location',
+        'price_tenge',
+        'added_timestamp',
+        'last_changed_admin',
+        'updated_timestamp',
+    ];
+
+    const UPDATED_AT = null; 
+    const CREATED_AT = null; 
+
+    public function university()
+    {
+        return $this->belongsTo(University::class, 'university_id');
+    }
 }
