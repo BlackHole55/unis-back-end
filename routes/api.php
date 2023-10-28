f70e0eceae93a0d2a3cfd02b417bf9f2b4a22771
Meirhan <syzdykovmr@gmail.com>
2023-10-28 20:20:21 +0600

    Dorms, Specialties and Universities routes

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UniversityController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\DormController;
+use App\Http\Controllers\SpecialityController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,16 +18,40 @@ use App\Http\Controllers\AdminController;
 |
 */
 
+//Public University routes
 Route::get('v1/universities', [UniversityController::class, 'index']);
 Route::get('v1/universities/{name}', [UniversityController::class, 'show']);
+
+//Public Admin routes
 Route::post('v1/auth/admin/signup', [AdminController::class, 'signup']);
 Route::post('v1/auth/admin/signin', [AdminController::class, 'signin']);
 
+//Public Dorm routes
+Route::get('v1/universities/dorms', [DormController::class, 'index']);
+Route::get('v1/universities/dorms/{id}', [DormController::class, 'show']);
+
+//Public Speciality routes
+Route::get('v1/specialties', [SpecialityController::class, 'index']);
+Route::get('v1/specialties/{id}', [SpecialityController::class, 'show']);
+
 Route::group(['middleware' => ['auth:sanctum']], function () {
+    //Protected University routes
     Route::post('v1/universities', [UniversityController::class, 'store']);
     Route::patch('v1/universities/{id}', [UniversityController::class, 'update']);
     Route::delete('v1/universities/{id}', [UniversityController::class, 'destroy']);
+
+    //Protected Admin routes
     Route::post('v1/auth/admin/signout', [AdminController::class, 'signout']);
+
+    //Protected Dorm routes
+    Route::post('v1/universities/{id}/dorms', [DormController::class, 'store']);
+    Route::patch('v1/universities/{id}/dorms', [DormController::class, 'update']);
+    Route::delete('v1/universities/dorms/{id}', [DormController::class, 'destroy']);
+
+    //Protected Speciality routes
+    Route::post('v1/specialties', [SpecialityController::class, 'store']);
+    Route::patch('v1/specialties/{id}', [SpecialityController::class, 'update']);
+    Route::delete('v1/specialties/{id}', [SpecialityController::class, 'destroy']);
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
